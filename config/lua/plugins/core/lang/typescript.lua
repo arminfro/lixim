33303db2a7d210fd43f6be21ab9a0967dc8a341d
Armin Fröhlich <mail@arminfroehlich.ch>
2025-01-22 12:03:41 +0100

    rm ts-standard linter

--- config/lua/plugins/core/lang/typescript.lua
+++ config/lua/plugins/core/lang/typescript.lua
@@ -1,17 +1,6 @@
 return {
 
   {
-    "mfussenegger/nvim-lint",
-    optional = true,
-    opts = {
-      linters_by_ft = {
-        typescript = { "ts-standard" },
-        typescriptreact = { "ts-standard" },
-      },
-    },
-  },
-
-  {
     "luckasRanarison/nvim-devdocs",
     optional = true,
     opts = { ensure_installed = { "typescript", "javascript", "dom", "eslint" } },
