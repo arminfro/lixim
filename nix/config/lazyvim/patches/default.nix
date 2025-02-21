8dd8d72c5a67492cdda3259e90b7fa7ce8c6592b
Armin Fröhlich <mail@arminfroehlich.ch>
2025-02-21 12:09:47 +0100

    make help buffers appear vertically splitted on the right

--- nix/config/lazyvim/patches/default.nix
+++ nix/config/lazyvim/patches/default.nix
@@ -2,5 +2,6 @@
 [
   ./adapt-prettier-to-fit-biome-usage.patch
   ./fix-js-debug-ref.patch
+  ./mv-help-integration-in-edgy-to-the-right.patch
 ]
 ++ lib.optional (config.enableLvl != "lazyvim") ./lualine.patch
