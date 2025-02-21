d70795d0c2df2326fbeaf51c7dfa500dd5738faa
Armin Fröhlich <mail@arminfroehlich.ch>
2025-02-21 11:48:45 +0100

    add autocmd autosave on TermEnter
    add auto-save on TermEnter

--- config/lua/config/autocmds.lua
+++ config/lua/config/autocmds.lua
@@ -2,30 +2,43 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 
-vim.api.nvim_create_autocmd({ "FocusLost" }, {
-  callback = function()
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-      if vim.api.nvim_get_option_value("modified", { buf = buf }) and vim.fn.getbufvar(buf, "&modifiable") ~= 0 then
-        vim.api.nvim_buf_call(buf, function()
-          vim.cmd([[silent! update]])
-        end)
-      end
+local function save_all_modifed_buffers()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_get_option_value("modified", { buf = buf }) and vim.fn.getbufvar(buf, "&modifiable") ~= 0 then
+      vim.api.nvim_buf_call(buf, function()
+        vim.cmd([[silent! update]])
+      end)
     end
-  end,
+  end
+end
+
+vim.api.nvim_create_autocmd({ "FocusLost" }, {
+  callback = save_all_modifed_buffers,
+  desc = "Save all modified buffers when loosing focus",
   pattern = "*",
-  group = augroup("autosave"),
+  group = augroup("autosave_on_FocusLost"),
 })
 
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  callback = function()
-    setCmpDictionaryByCurrentLanguages(vim.opt.spelllang:get())
-  end,
+vim.api.nvim_create_autocmd({ "TermEnter" }, {
+  callback = save_all_modifed_buffers,
+  desc = "Save all modified buffers when entering Terminal",
   pattern = "*",
-  group = augroup("set_cmp_dictionary"),
+  group = augroup("autosave_on_TermEnter"),
 })
+
+-- todo, update blink with configured dictionary provider
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--   desc = "Set spelllang for completion",
+--   callback = function()
+--     setCmpDictionaryByCurrentLanguages(vim.opt.spelllang:get())
+--   end,
+--   pattern = "*",
+--   group = augroup("set_cmp_dictionary"),
+-- })
 
 -- quit some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
+  desc = "Quit some filetypes easily with q",
   group = augroup("quit_with_q"),
   pattern = {
     "dap-float",
