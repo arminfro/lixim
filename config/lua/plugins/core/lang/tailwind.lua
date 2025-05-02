b64c2bcdca03177f4793d14917cbda6434ddf476
Armin Fröhlich <mail@arminfroehlich.ch>
2025-05-02 17:15:57 +0200

    udpate lang.tailwind config

--- config/lua/plugins/core/lang/tailwind.lua
+++ config/lua/plugins/core/lang/tailwind.lua
@@ -1,5 +1,29 @@
 return {
   {
+    "luckasRanarison/tailwind-tools.nvim",
+    build = ":UpdateRemotePlugins",
+    event = "LazyFile",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      { "nvim-telescope/telescope.nvim", optional = true },
+      { "neovim/nvim-lspconfig", optional = true },
+    },
+    opts = {},
+    config = function(_, opts)
+      require("tailwind-tools").setup(opts)
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typescriptreact = { "rustywind" },
+      },
+    },
+  },
+
+  {
     "luckasRanarison/nvim-devdocs",
     optional = true,
     opts = { ensure_installed = { "tailwindcss" } },
--- nix/config/core/lang/tailwind.nix
+++ nix/config/core/lang/tailwind.nix
@@ -1,8 +1,15 @@
 self:
 {
+  pkgs,
   ...
 }:
 {
+  extraPackages = [ pkgs.rustywind ];
+
+  plugins = with pkgs.vimPlugins; [
+    tailwind-tools-nvim
+  ];
+
   extraLazyImport = [
     "plugins.core.lang.tailwind"
   ];
